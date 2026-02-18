0e0ef865a5790632300b5d293e74932f3e419645
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-18 19:43:32 -0300

    DIA 001

---
+++ up_mapas/descobrir_hp.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+time.sleep(5)  # tempo pra você posicionar o mouse na barra de HP
+print(pyautogui.position())
+
+# começo da barra = (x=82, y=49)
+# final da barra = (x=276, y=49)
